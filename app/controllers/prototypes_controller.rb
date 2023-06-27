911d63b9fbf577287cd5fe562b7cd484b0467765
kazumakabashima <k.kazuma1028@gmail.com>
2023-06-27 15:38:55 +0900

    編集機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,9 +25,12 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else 
+      render :edit
+    end
   end
 
   private
